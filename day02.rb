9e59dcba3448b1e164207a12b32c797a3174b98b
Dmitri Tchebotarev <dmitri@tchebotarev.com>
2023-12-02 12:59:40 -0500

    Even more ruby

--- day02.rb
+++ day02.rb
@@ -4,8 +4,5 @@ games = input.each_with_index.map do |line, number|
   [number+1, ['r','g','b'].map{|k| line.scan(/(\d+) #{k}/).flatten.map(&:to_i).max}]
 end.to_h
 
-games.select do |game_num, game|
-  game[0] <= 12 && game[1] <= 13 && game [2] <= 14
-end.keys.sum.then { |s| puts s }
-
+puts games.select{|_, g| g[0] <= 12 && g[1] <= 13 && g[2] <= 14}.keys.sum
 puts games.values.map{|i| i.reduce(&:*)}.sum
