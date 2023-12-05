662e0ad92054a64be51ee1c515360d506329ff07
Dmitri Tchebotarev <dmitri@tchebotarev.com>
2023-12-04 23:50:23 -0500

    Refactor

--- day04.rb
+++ day04.rb
@@ -1,18 +1,12 @@
 input = File.read('inputs/day04').split("\n")
 
-input.map do |line|
+cards = [1] * input.length
+input.map.with_index do |line, game_num|
   (winning, card) = line.split(':')[1].split('|').map{ |l| l.scan(/\d+/).map(&:to_i) }
   winners = card.filter{|c| winning.include? c}
-  winners.size > 0 ? 2**(winners.size - 1) : 0
-end.sum.then{|l| puts l}
-
-cards = [1] * input.length
-
-input.each_with_index do |line, game_num|
-  (winning, card) = line.split(':')[1].split('|').map{ |l| l.scan(/\d+/).map(&:to_i) }
-  card.filter{|c| winning.include? c}.each_with_index do |_, i|
+  winners.each_with_index do |_, i|
     cards[game_num + i + 1] += cards[game_num]
   end
-end
-
+  winners.size > 0 ? 2**(winners.size - 1) : 0
+end.sum.then{|l| puts l}
 puts cards.sum
